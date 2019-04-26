9e70b2c3cbfe1555466ae6c41c98dc98bc9a80b0
Ben Leggiero <BenLeggiero@Gmail.com>
2019-04-26 19:58:46 -0400

    Initial Commit

---
+++ PixelArtKit/PixelArtKit.h
@@ -0,0 +1,19 @@
+//
+//  PixelArtKit.h
+//  PixelArtKit
+//
+//  Created by Ben Leggiero on 4/26/19.
+//  Copyright © 2019 Ben Leggiero. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PixelArtKit.
+FOUNDATION_EXPORT double PixelArtKitVersionNumber;
+
+//! Project version string for PixelArtKit.
+FOUNDATION_EXPORT const unsigned char PixelArtKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PixelArtKit/PublicHeader.h>
+
+
